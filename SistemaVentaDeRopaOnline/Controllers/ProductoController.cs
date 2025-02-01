fcb2fb3195bbc17e703133a808c1eff8c15f2ffb
andycvv <135892408+andycvv@users.noreply.github.com>
2025-02-01 10:42:00 -0500

    feat: agregar vista detalle para cada producto con botones para seleccionar talla, color y agregar al carrito

--- SistemaVentaDeRopaOnline/Controllers/ProductoController.cs
+++ SistemaVentaDeRopaOnline/Controllers/ProductoController.cs
@@ -23,6 +23,15 @@ namespace SistemaVentaDeRopaOnline.Controllers
             ViewBag.genero = genero;
             return View(productos);
         }
+        public async Task<IActionResult> Detalle(int id, int? ideTal)
+        {
+            var producto = await GetProduct(id, ideTal);
+            if (ideTal != null)
+            {
+                ViewBag.IdeTal = ideTal;
+            }
+            return View(producto);
+        }
         private async Task<List<Producto>> GetAllProducts(string? genero)
         {
             var query = context.Productos
@@ -36,6 +45,25 @@ namespace SistemaVentaDeRopaOnline.Controllers
             }
 
             return await query.ToListAsync();
+        }
+        private async Task<Producto?> GetProduct(int id, int? ideTal)
+        {
+            var query = context.Productos
+                .Include(p => p.ImagenProductos)
+                .Include(p => p.Inventarios)
+                    .ThenInclude(i => i.Talla)
+                .Include(p => p.Inventarios)
+                    .ThenInclude(i => i.Color)
+                .Where(p => p.Id == id)
+                .AsQueryable();
+
+            if (ideTal.HasValue)
+            {
+                query = query
+                    .Where(p => p.Inventarios.Any(i => i.TallaId == ideTal.Value));
+            }
+
+            return await query.FirstOrDefaultAsync();
         }
     }
 }
