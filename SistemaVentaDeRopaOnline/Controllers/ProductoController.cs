57f8bedaca5085d92c05c4ea225937b85aee20ba
andycvv <135892408+andycvv@users.noreply.github.com>
2025-02-01 09:57:26 -0500

    feat: agrega ProductoController con la estructura básica

---
+++ SistemaVentaDeRopaOnline/Controllers/ProductoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using SistemaVentaDeRopaOnline.Data;
+using SistemaVentaDeRopaOnline.Models;
+
+namespace SistemaVentaDeRopaOnline.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly SistemaContext context;
+        public ProductoController(SistemaContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
