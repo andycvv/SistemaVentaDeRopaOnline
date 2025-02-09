1eeb335fa1a9baea022f59176fb3a7890b25d078
Yadira Cceline Auccasi Loa <I202311686@cibertec.edu.pe>
2025-02-09 16:38:02 -0500

    feat: agregar propiedad Estado al Modelo Talla

--- SistemaVentaDeRopaOnline/Models/Talla.cs
+++ SistemaVentaDeRopaOnline/Models/Talla.cs
@@ -10,8 +10,11 @@ public class Talla
 {
     public int Id { get; set; }
 
-    [StringLength(30)]
+    [Required(ErrorMessage = "Debe escribir el nombre de la talla.")]
+    [StringLength(10, ErrorMessage = "El nombre no debe superar los 30 caracteres.")]
+    [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "El nombre solo puede contener letras, números y espacios.")]
     public string Nombre { get; set; } = null!;
+    public bool Estado { get; set; } = true;
 
     public List<DetallePedido> DetallePedidos { get; set; } = new List<DetallePedido>();
     public List<Inventario> Inventarios { get; set; } = new List<Inventario>();
