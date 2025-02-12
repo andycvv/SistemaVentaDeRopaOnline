88f9fa87eb9bc2e52133d79e5149db4a128c329b
Jose Martin J <majac@LAPTOP-NUFGG3PU>
2025-02-12 01:07:32 -0500

    feat: Agregar CRUD de Usuario

---
+++ SistemaVentaDeRopaOnline/Controllers/UsuarioController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaVentaDeRopaOnline.Data;
+using SistemaVentaDeRopaOnline.Models;
+
+namespace SistemaVentaDeRopaOnline.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly SistemaContext _context;
+
+        public UsuarioController(SistemaContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Listar()
+        {
+            var usuarios = await _context.Usuarios.ToListAsync();
+            return View(usuarios);
+        }
+
+        public IActionResult Crear() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Usuarios.Add(usuario);
+                await _context.SaveChangesAsync();
+                CrearAlerta("success", "Se registró el usuario correctamente");
+                return RedirectToAction("Listar");
+            }
+            return View(usuario);
+        }
+
+        public async Task<IActionResult> Editar(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(int id, Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(usuario);
+                await _context.SaveChangesAsync();
+                CrearAlerta("success", "Se editó el usuario correctamente");
+                return RedirectToAction("Listar");
+            }
+            return View(usuario);
+        }
+
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var usuario = await _context.Usuarios.FindAsync(id);
+            if (usuario != null)
+            {
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+                CrearAlerta("success", "Se eliminó el usuario correctamente");
+            }
+            else
+            {
+                CrearAlerta("error", "El usuario no existe");
+            }
+
+            return RedirectToAction("Listar");
+        }
+
+        public void CrearAlerta(string alertType, string alertMessage)
+        {
+            TempData["AlertMessage"] = alertMessage;
+            TempData["AlertType"] = alertType;
+        }
+    }
+}
