2899a5e8925f8482bcbd69f876ccb3cb3da42ea1
Yadira Cceline Auccasi Loa <I202311686@cibertec.edu.pe>
2025-02-13 16:34:24 -0500

    feat: corregir mensaje de validación del Modelo Talla

--- SistemaVentaDeRopaOnline/Models/Talla.cs
+++ SistemaVentaDeRopaOnline/Models/Talla.cs
@@ -10,8 +10,8 @@ public class Talla
 {
     public int Id { get; set; }
 
-    [Required(ErrorMessage = "Debe escribir el nombre de la talla.")]
-    [StringLength(10, ErrorMessage = "El nombre no debe superar los 30 caracteres.")]
+    [Required(ErrorMessage = "Ingrese el nombre de la talla.")]
+    [StringLength(30, ErrorMessage = "El nombre no debe superar los 30 caracteres.")]
     [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "El nombre solo puede contener letras, números y espacios.")]
     public string Nombre { get; set; } = null!;
     public bool Estado { get; set; } = true;
