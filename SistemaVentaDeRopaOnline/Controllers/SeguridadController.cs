a2cffb553e10d5e29085aff3a3586fb0403b1d05
Andy Vargas <andymanquues@outlook.com.pe>
2025-02-13 18:27:39 -0500

    feat: crear SeguridadController con la estructura basica

---
+++ SistemaVentaDeRopaOnline/Controllers/SeguridadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVentaDeRopaOnline.Models;
+using SistemaVentaDeRopaOnline.Models.ViewModels;
+
+namespace SistemaVentaDeRopaOnline.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly SignInManager<Usuario> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeguridadController
+        (
+            UserManager<Usuario> userManager, 
+            SignInManager<Usuario> signInManager, 
+            RoleManager<IdentityRole> roleManager
+        )
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+    }
+}
