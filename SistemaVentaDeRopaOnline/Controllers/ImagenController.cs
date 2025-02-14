e5f98cdecf959ef36fe988497316d9c9f01037a4
Yadira Cceline Auccasi Loa <I202311686@cibertec.edu.pe>
2025-02-14 16:10:14 -0500

    feat: agregar controlador de Imagen con la estructura básica

---
+++ SistemaVentaDeRopaOnline/Controllers/ImagenController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVentaDeRopaOnline.Data;
+
+namespace SistemaVentaDeRopaOnline.Controllers
+{
+    public class ImagenController : Controller
+    {
+        private readonly SistemaContext _sistemaContext;
+        public ImagenController(SistemaContext sistemaContext)
+        {
+            _sistemaContext = sistemaContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
