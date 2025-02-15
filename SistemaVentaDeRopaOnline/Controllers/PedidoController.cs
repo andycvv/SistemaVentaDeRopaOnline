8d707c3ce5a0275a8c6e1b84cd3f8a96a5a8a160
Andy Vargas <andymanquues@outlook.com.pe>
2025-02-15 10:57:15 -0500

    feat: agregar metodo y vista Index en el PedidoController

--- SistemaVentaDeRopaOnline/Controllers/PedidoController.cs
+++ SistemaVentaDeRopaOnline/Controllers/PedidoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using SistemaVentaDeRopaOnline.Data;
 using SistemaVentaDeRopaOnline.Models;
 
@@ -15,9 +16,39 @@ namespace SistemaVentaDeRopaOnline.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var usuario = await _userManager.GetUserAsync(User);
+
+            if (usuario == null)
+            {
+                CrearAlerta("error", "Debe ingresar con su cuenta para realizar un pedido.");
+                return RedirectToAction("Index", "Producto");
+            }
+
+            var pedido = await context.Pedidos
+                .Include(p => p.DetallePedidos)
+                    .ThenInclude(d => d.Inventario)
+                    .ThenInclude(i => i.Producto)
+                    .ThenInclude(p => p.ImagenProductos)
+                .Include(p => p.DetallePedidos)
+                    .ThenInclude(d => d.Inventario)
+                    .ThenInclude(i => i.Color)
+                .Include(p => p.DetallePedidos)
+                    .ThenInclude(d => d.Inventario)
+                    .ThenInclude(i => i.Talla)
+                .Where(p => p.UsuarioId == u.Id)
+                .FirstOrDefaultAsync();
+
+            if (pedido == null) pedido = new Pedido();
+
+            return View(pedido);
+        }
+
+        public void CrearAlerta(string alertType, string alertMessage)
+        {
+            TempData["AlertMessage"] = alertMessage;
+            TempData["AlertType"] = alertType;
         }
     }
 }
