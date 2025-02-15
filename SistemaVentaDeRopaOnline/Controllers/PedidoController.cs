717a3e984ae57e2148e94db81dd9e089d7edce7b
Andy Vargas <andymanquues@outlook.com.pe>
2025-02-14 21:21:33 -0500

    feat: agregar PedidoController con la estructura básica

---
+++ SistemaVentaDeRopaOnline/Controllers/PedidoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVentaDeRopaOnline.Data;
+using SistemaVentaDeRopaOnline.Models;
+
+namespace SistemaVentaDeRopaOnline.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly SistemaContext context;
+        private readonly UserManager<Usuario> _userManager;
+        public PedidoController(UserManager<Usuario> userManager, SistemaContext context)
+        {
+            this.context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
