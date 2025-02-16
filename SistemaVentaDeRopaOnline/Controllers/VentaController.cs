6496e02809fc1774119ea00306e93ac95ea7f022
Andy Vargas <andymanquues@outlook.com.pe>
2025-02-15 19:19:09 -0500

    feat: agregar VentaController con la estructura básica

---
+++ SistemaVentaDeRopaOnline/Controllers/VentaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVentaDeRopaOnline.Data;
+using SistemaVentaDeRopaOnline.Models;
+
+namespace SistemaVentaDeRopaOnline.Controllers
+{
+    public class VentaController : Controller
+    {
+        private readonly SistemaContext context;
+        public VentaController(SistemaContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
