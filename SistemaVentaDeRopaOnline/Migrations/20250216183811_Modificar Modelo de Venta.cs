8e2cb3f02e6bb76b6c37623f9ff2993dff996845
Andy Vargas <andymanquues@outlook.com.pe>
2025-02-16 14:49:15 -0500

    feat: agregar migracion "Modificar modelo de Vemta"

---
+++ SistemaVentaDeRopaOnline/Migrations/20250216183811_Modificar Modelo de Venta.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaVentaDeRopaOnline.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModificarModelodeVenta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MetodoPago",
+                table: "Ventas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Apellido",
+                table: "Ventas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Correo",
+                table: "Ventas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DNI",
+                table: "Ventas",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Direccion",
+                table: "Ventas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "Ventas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefono",
+                table: "Ventas",
+                type: "nvarchar(9)",
+                maxLength: 9,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TipoComprobante",
+                table: "Ventas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apellido",
+                table: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "Correo",
+                table: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "DNI",
+                table: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "Direccion",
+                table: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "Telefono",
+                table: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "TipoComprobante",
+                table: "Ventas");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MetodoPago",
+                table: "Ventas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
