3facb37c371a7c83727538e3089560162206c814
Yadira Cceline Auccasi Loa <I202311686@cibertec.edu.pe>
2025-02-18 00:04:04 -0500

    feat: agregar estructura básica de InventarioController

--- SistemaVentaDeRopaOnline/Controllers/InventarioController.cs
+++ SistemaVentaDeRopaOnline/Controllers/InventarioController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SistemaVentaDeRopaOnline.Data;
 
 namespace SistemaVentaDeRopaOnline.Controllers
 {
     public class InventarioController : Controller
     {
+        private readonly SistemaContext _sistemaContext;
+        public InventarioController(SistemaContext sistemaContext)
+        { 
+            _sistemaContext = sistemaContext;
+        }
+
         public IActionResult Index()
         {
             return View();
