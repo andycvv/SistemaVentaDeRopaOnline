3151a5c6cf011ed82c088b13a03bac6df7ba28e7
Yadira Cceline Auccasi Loa <I202311686@cibertec.edu.pe>
2025-02-19 13:18:32 -0500

    feat: agregar configuración de Swagger

--- SistemaVentaDeRopaOnline/Program.cs
+++ SistemaVentaDeRopaOnline/Program.cs
@@ -30,6 +30,9 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequiredUniqueChars = 1;
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -45,6 +48,8 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
