deec8c930628559e583e2d9911a251fff02c254c
Andy Vargas <andymanquues@outlook.com.pe>
2025-02-20 21:52:33 -0500

    feat: crear ventaDTO

---
+++ SistemaVentaDeRopaOnline/Models/DTO/VentaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemaVentaDeRopaOnline.Models.DTO
+{
+    public class VentaDTO
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string MetodoPago { get; set; } = null!;
+        public string Correo { get; set; } = null!;
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; } = null!;
+        public string Direccion { get; set; } = null!;
+        public string DNI { get; set; } = null!;
+        public string Telefono { get; set; } = null!;
+        public string TipoComprobante { get; set; } = null!;
+        public int PedidoId { get; set; }
+    }
+}
